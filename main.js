c6e27882c27e2ad298e626835f70a2a2c83ae3c7
Sunandhit-Gupta <sunandhitgupta@gmail.com>
2023-01-28 15:14:27 +0530

    First

---
+++ main.js
@@ -0,0 +1,63 @@
+
+
+url1 = 'https://call.callbomberz.in/?phn=45385041384f5959697854493468474169672b6a78773d3d'   //Aarav
+url2 ='https://call.callbomberz.in/?phn=5a6c43554d7a556b64594c72495835436968613666773d3d'  //Rhythm
+url3 = 'https://ccall.callbomberz.in/?phn=4b4b6f5664713842313141514d734768597639592b513d3d' // ~&
+url4 = 'https://call.callbomberz.in/?phn=68673646446c4558644e2b56506947316e4f396a7a773d3d' //Ansh
+url5 = 'https://call.callbomberz.in/?phn=4e4c57354c3876374f53786c5273636b544b4e3468773d3d' //Jaspreet
+url6 = 'https://calll.callbomberz.in/?phn=746b5a516b5439484a39776c65516d54304461524d773d3d' //~(._.)~
+url7 = 'https://ccall.callbomberz.in/?phn=6d6149335951353838617a3463703950646b4b7a58773d3d' //Raghuvanshi
+
+
+function bomb1(){
+
+    window.open(url1,"_blank");
+
+}
+function bomb2(){
+    window.open(url2,"_blank");
+}
+
+function bomb3(){
+    window.open(url3,"_blank");
+}
+function bomb4(){
+    window.open(url4,"_blank");
+}
+function bomb5(){
+    window.open(url5,"_blank");
+}
+function bomb6(){
+    window.open(url6,"_blank");
+}
+function bomb7(){
+    window.open(url7,"_blank");
+}
+// function bombAll(){
+//     window.open(url1,"_blank");
+    
+//     window.open(url2,"_blank");
+//     window.open(url3,"_blank");
+//     window.open(url4,"_blank");
+//     window.open(url5,"_blank");
+//     window.open(url6,"_blank");
+//     window.open(url7,"_blank");
+// }
+
+function bombAll(){
+
+    bomb1();
+    setTimeout(bomb2(),3000);
+    bomb2();
+    setTimeout(bomb3(),3000);
+    bomb3();
+    setTimeout(bomb4(),3000);
+    bomb4();
+    setTimeout(bomb5(),3000);
+    bomb5();
+    setTimeout(bomb6(),3000);
+    bomb6();
+    setTimeout(bomb7(),3000);
+    bomb7();
+
+}
